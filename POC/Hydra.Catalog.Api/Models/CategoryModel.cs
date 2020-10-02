cb652324c3c66621d97d0f9676bdd5259caa955e
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2020-10-02 11:39:01 +0100

    remove poc folder

--- POC/Hydra.Catalog.Api/Models/CategoryModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Hydra.Catalog.Api.Models
-{
-    public class CategoryModel
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
--- POC/Hydra.Catalog.PriceUpdate/Models/ProductDetail.cs
+++
@@ -1,9 +0,0 @@
-namespace Hydra.Catalog.PriceUpdate.Models
-{
-    public class ProductDetail
-    {
-        public int Id { get; set; }
-        public decimal OldPrice { get; set; }
-        public decimal NewPrice { get; set; }
-    }
-}
